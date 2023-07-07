4d38063741cf6cdaeb0b846e413b57b1557b19a1
Laura <tomcatthenommer@gmail.com>
2023-07-07 20:18:19 +1000

    fixed the regex but it's not grabbing from the whole string for some reason

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import urllib.request as reader
 import re
 
+cardsCountDictionary = dict
+
 page = reader.urlopen("https://melee.gg/Decklist/View/309545")
 
 temp = ""
 for i in page:
     temp += str(i)
 
-#print(temp)
-print(type(temp))
-cards = re.findall("/b'\d ([^\\r]*)\\r/g", temp)
+print(temp)
+#print(type(temp))
+pattern = r"b'(\d) ([^\\r]*)\\r"
+cards = re.findall(pattern, temp)
 
-print(cards)
+#print(cards)
 for i in cards:
     print(i)
